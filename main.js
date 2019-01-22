69777e94caae588e0f074852ac6cf6605b44a9f4
MatteoGodzilla <32652410+MatteoGodzilla@users.noreply.github.com>
2019-01-22 11:57:10 +0100

    added cells at work

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+let matrix = new Array(5); //colonne
+let padding = 20;
+
 function setup() {
-    createCanvas(800, 600)
+	createCanvas(600, 600);
+	for(let cols = 0; cols < matrix.length;cols++){
+		matrix[cols] = new Array(10); //righe
+	}
+	for(let cols = 0; cols < matrix.length;cols++){
+		for(let rows = 0; rows < matrix[cols].length;rows++){
+			matrix[cols][rows] = new cell(cols,rows,0)
+		}
+	}
 }
 
+
+
 function draw(){
-    background(51);
+	background(51);
+	for(let cols = 0; cols < matrix.length; cols++){
+		for(let rows = 0; rows < matrix[0].length;rows++){
+			push();
+				translate(padding,padding);
+				matrix[cols][rows].show();
+			pop();
+		}
+	} 
 }
